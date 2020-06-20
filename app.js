4b1fcbcec58470edbb0ab327ab9d2e4a2d103913
Dennis Flannigan <DLFlann@gmail.com>
2020-06-19 19:11:16 -0500

    setup initial module layout

---
+++ app.js
@@ -0,0 +1,63 @@
+var budgetController = (function() {}
+
+)();
+
+var UIController = (function() {
+
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    };
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(e) { if (e.keyCode === 13 || e.which === 13) ctrlAddItem(); });
+    };
+
+    var ctrlAddItem = function() {
+
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calulate the budget
+
+        // 5. Display the budget on the UI
+    };
+
+    return {
+        init: function() {
+            setupEventListeners();
+        }
+    };
+
+})(budgetController, UIController);
+
+controller.init();
